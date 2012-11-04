95d28f173b62f37e5fe3eaebf9f870f1c2a921d2
Rastagong <Rastagong@c72a20b9-a0ed-4f72-bd54-0d040de82296>
2012-11-04 13:27:14 +0000

    New 0.1 build: had forgotten the regions image (sigh)...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["tkinter", "PIL","argparse"], "excludes": [], "icon": "NuvolaTileIcon.ico", "include_files":[("NuvolaTileIcon.ico","NuvolaTileIcon.ico")]}
+build_exe_options = {"packages": ["tkinter", "PIL","argparse"], "compressed":"True", "icon": "NuvolaTileIcon.ico", "include_files":[("NuvolaTileIcon.ico","NuvolaTileIcon.ico"), ("automappingRegions.png", "automappingRegions.png")]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
